fe38bc00ae04405d95d5a44031d6b673e783fab6
Brett David Silverman <brettdavidsilverman@gmail.com>
2020-03-23 20:10:45 +0000

    Fine tuning

---
+++ cgi/socket/config.h
@@ -0,0 +1,7 @@
+#ifndef SECURE_CONFIG_H
+#define SECURE_CONFIG_H
+
+const char Server::cert_pem[] = "/etc/letsencrypt/live/bee.fish/fullchain.pem";
+const char Server::key_pem[]  = "/etc/letsencrypt/live/bee.fish/privkey.pem";
+
+#endif
