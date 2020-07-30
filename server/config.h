0d46521cbe8655eb1a28758ee5e43a4e10d82585
Brett <brettdavidsilverman@gmail.com>
2020-07-30 14:36:17 +1000

    modified dhparan in config.h

--- server/config.h
+++ server/config.h
@@ -3,6 +3,6 @@
 
 #define KEY_FILE "/etc/letsencrypt/live/bee.fish/privkey.pem"
 #define CERT_FILE "/etc/letsencrypt/live/bee.fish/fullchain.pem"
-#define TMP_DH_FILE "/etc/ssl/certs/dhparam.pem"
+#define TMP_DH_FILE "dh2048.pem"
 
 #endif
