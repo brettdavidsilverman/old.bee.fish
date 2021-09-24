7fa639c52ad6cab9402934cd4828b202aab1df0f
Brett David Silverman <brettdavidsilverman@gmail.com>
2021-09-24 18:41:23 +1000

    wip

--- Arduino/m5-timer-cam/m5-timer-cam.ino
+++ Arduino/m5-timer-cam/m5-timer-cam.ino
@@ -1,4 +1,3 @@
-#include "esp_camera.h"
 #include <Arduino.h>
 #include <Wire.h>
 #include <WiFi.h>
@@ -13,23 +12,17 @@
 
 #include "esp_http_server.h"
 #include "esp_timer.h"
-//#include "esp_camera.h"
 #include "img_converters.h"
 
+#include "esp_camera.h"
 #include "camera_pins.h"
 #include "battery.h"
 #include "led.h"
-#include "Adafruit_MCP23008.h"
-Adafruit_MCP23008 mcp;
-bool lightOn = false;
-
-#define LIGHT_PIN 0     // MCP23XXX pin LED is attached to
-
+#include "light.h"
 #include "bme280i2c.h"
 
-BME280I2C bme;
-
-//#define BLUETOOTH
+#define BLUETOOTH
+//#define PSRAM
 
 #ifdef BLUETOOTH
 #include "BluetoothSerial.h"
@@ -41,6 +34,8 @@ BME280I2C bme;
 BluetoothSerial SerialBT;
 #endif
 
+
+#ifdef PSRAM
 bool psramInitialized = false;
 
 #undef malloc
@@ -52,12 +47,31 @@ void* malloc(size_t size) {
     return ::heap_caps_malloc(size, MALLOC_CAP_8BIT);
 }
 
+#endif
+
+void initializeBattery();
+void initializeLight();
+void initializeCamera();
+#ifdef PSRAM
+void initializeMemory();
+#endif
+
+void initializeLED();
+void initializeWiFi();
+void initializeWeather();
+void initializeWebServer();
 
 const char* ssid = "Bee";
 const char* password = "feebeegeeb3";
+//const char* ssid = "Telstra044F87";
+//const char* password = "ugbs3e85p5";
 
-void startCameraServer(); 
+httpd_handle_t camera_http_handle = NULL;
+Light* light;
+BME280I2C bme;
 
+
+void startCameraServer();
 
 void setup() {
 
@@ -68,21 +82,25 @@ void setup() {
 
 
 #ifdef BLUETOOTH  
-  SerialBT.begin("m5-timer-cam"); //Bluetooth device name
+  SerialBT.begin(String("feebeecam")); //Bluetooth device name
 #endif
 
 
   initializeBattery();
   initializeLight();
   initializeCamera();
+#ifdef PSRAM
   initializeMemory();
+#endif
+
   initializeLED();
   initializeWiFi();
   initializeWeather();
+  //initializeWebServer();
+  startCameraServer();
 
   led_brightness(1024);
   
-  startCameraServer();
 
   Serial.print("Ready! Use 'http://");
   Serial.print(WiFi.localIP());
@@ -97,8 +115,10 @@ void loop() {
 
   if ((millis() - lastTime) >= 5000) {
     
-    if (Serial.available())
+    if (Serial.available()) {
+      Serial.print("Restarting...");
       ESP.restart();
+    }
 
     lastTime = millis();
     // put your main code here, to run repeatedly:
@@ -120,13 +140,12 @@ Stream* client;
     printCPUData(client);
 
   }
+
   
-
-  //delay(10000);
-
 }
 
 
+#ifdef PSRAM
 void initializeMemory() {
 
   if (psramFound()) {
@@ -142,6 +161,7 @@ void initializeMemory() {
   }
 
 }
+#endif
 
 void initializeCamera() {
 
@@ -181,11 +201,12 @@ void initializeCamera() {
   sensor_t * s = esp_camera_sensor_get();
   //initial sensors are flipped vertically and colors are a bit saturated
   s->set_vflip(s, 1);//flip it back
+  s->set_hmirror(s, 1);
   s->set_brightness(s, 1);//up the blightness just a bit
   s->set_saturation(s, -2);//lower the saturation
 
   //drop down frame size for higher initial frame rate
-  s->set_framesize(s, FRAMESIZE_QVGA);
+  s->set_framesize(s, FRAMESIZE_SVGA);
 
 }
 
@@ -213,15 +234,7 @@ void initializeWiFi() {
 }
 
 void initializeLight() {
-
-    mcp.begin();
-
-    // configure pin for output
-    mcp.digitalWrite(LIGHT_PIN, LOW);
-    mcp.pinMode(LIGHT_PIN, OUTPUT);
-    mcp.digitalWrite(LIGHT_PIN, HIGH);
-    lightOn = false;
-
+    light = new Light();
 }
 
 
@@ -270,93 +283,56 @@ void printCPUData(Stream* client) {
 
 }
 
+
+void initializeWebServer() {
+
+  httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+
+
+  httpd_uri_t get_image_uri = {
+    .uri       = "/image",
+    .method    = HTTP_GET,
+    .handler   = get_image_handler,
+    .user_ctx  = NULL
+  };
+
+  httpd_uri_t get_stream_uri = {
+    .uri       = "/stream",
+    .method    = HTTP_GET,
+    .handler   = get_stream_handler,
+    .user_ctx  = NULL
+  };
+
+  config.server_port = 80;
+  Serial.printf("Starting server on port: '%d'\n", config.server_port);
+  if (httpd_start(&camera_http_handle, &config) == ESP_OK) {
+    httpd_register_uri_handler(camera_http_handle, &get_stream_uri);
+    httpd_register_uri_handler(camera_http_handle, &get_image_uri);
+    Serial.println("Camera Ready! Use: ");
+    Serial.print("http://");
+    Serial.print(WiFi.localIP());
+    //Serial.print(IP);
+    Serial.print("/image");
+    Serial.println("");
+    Serial.print("http://");
+    Serial.print(WiFi.localIP());
+    //Serial.print(IP);
+    Serial.print("/stream");
+    Serial.println("");
+  }
+  else
+    Serial.println("Error starting server");
+
+  led_brightness(0);
+
+}
+
 static const char *TAG = "example:take_picture";
-
-static camera_config_t camera_config;
-
-#define BYTES_PER_PIXEL 3
 
 #define PART_BOUNDARY "123456789000000000000987654321"
 static const char* _STREAM_CONTENT_TYPE = "multipart/x-mixed-replace;boundary=" PART_BOUNDARY;
 static const char* _STREAM_BOUNDARY = "\r\n--" PART_BOUNDARY "\r\n";
 static const char* CONTENT_TYPE = "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";
-
-void startCameraServer();
-
-static esp_err_t init_camera()
-{
-    camera_config_t& config = camera_config;
-
-    //initialize the camera
-    config.ledc_channel = LEDC_CHANNEL_0;
-    config.ledc_timer = LEDC_TIMER_0;
-    config.pin_d0 = Y2_GPIO_NUM;
-    config.pin_d1 = Y3_GPIO_NUM;
-    config.pin_d2 = Y4_GPIO_NUM;
-    config.pin_d3 = Y5_GPIO_NUM;
-    config.pin_d4 = Y6_GPIO_NUM;
-    config.pin_d5 = Y7_GPIO_NUM;
-    config.pin_d6 = Y8_GPIO_NUM;
-    config.pin_d7 = Y9_GPIO_NUM;
-    config.pin_xclk = XCLK_GPIO_NUM;
-    config.pin_pclk = PCLK_GPIO_NUM;
-    config.pin_vsync = VSYNC_GPIO_NUM;
-    config.pin_href = HREF_GPIO_NUM;
-    config.pin_sscb_sda = SIOD_GPIO_NUM;
-    config.pin_sscb_scl = SIOC_GPIO_NUM;
-    config.pin_pwdn = PWDN_GPIO_NUM;
-    config.pin_reset = RESET_GPIO_NUM;
-    config.xclk_freq_hz = 20000000;
-    config.pixel_format = PIXFORMAT_JPEG;
-    config.frame_size = FRAMESIZE_UXGA;
-    config.jpeg_quality = 10;
-    config.fb_count = 2;
-
-    esp_err_t err = esp_camera_init(&camera_config);
-    if (err != ESP_OK)
-    {
-        ESP_LOGE(TAG, "Camera Init Failed");
-        return err;
-    }
-    sensor_t * s = esp_camera_sensor_get();
-    //initial sensors are flipped vertically and colors are a bit saturated
-    s->set_vflip(s, 1);//flip it back
-    s->set_hmirror(s, 1);
-    s->set_brightness(s, 1);//up the blightness just a bit
-    s->set_saturation(s, -2);//lower the saturation
-
-    //drop down frame size for higher initial frame rate
-    s->set_framesize(s, FRAMESIZE_SXGA);
-
-    return ESP_OK;
-}
-*/
-
-/*
-httpd_handle_t camera_http_handle = NULL;
-
-typedef struct {
-        size_t size; //number of values used for filtering
-        size_t index; //current value index
-        size_t count; //value count
-        int sum;
-        int * values; //array to be filled with values
-} ra_filter_t;
-
-
-byte checkEndTransmission(const char* message) {
-  byte result = Wire.endTransmission();
-  if (result == 0) {
-     //Serial.println("Ok");
-  }
-  else {
-    Serial.print(message);
-    Serial.print(" error ");
-    Serial.println(result);
-  }
-
-  return result;
-}
 
 static esp_err_t get_image_handler(httpd_req_t *req){
     camera_fb_t * fb = NULL;
@@ -371,15 +347,14 @@ static esp_err_t get_image_handler(httpd_req_t *req){
 
     sensor_t * s = esp_camera_sensor_get();
     
-    s->set_quality(s, 10);
-    s->set_framesize(s, FRAMESIZE_SXGA);
+    //s->set_quality(s, 10);
+    s->set_framesize(s, FRAMESIZE_UXGA);
 
-    //pinMode(FLASH_LED, OUTPUT);
-    //digitalWrite(FLASH_LED, HIGH);
+    light->turnOn();
 
     fb = esp_camera_fb_get();
     
-    //digitalWrite(FLASH_LED, LOW);
+    light->turnOff();
 
     if (!fb) {
         Serial.println("Camera capture failed");
@@ -415,11 +390,12 @@ static esp_err_t get_image_handler(httpd_req_t *req){
     int64_t frame_time = fr_end - last_frame;
     last_frame = fr_end;
     frame_time /= 1000;
+    
     Serial.printf(
       "(%.1ffps)\n",
       1000.0 / (uint32_t)frame_time
     );
-
+    
     return res;
 }
 
@@ -450,28 +426,10 @@ esp_err_t takePicture() {
         res = ESP_FAIL;
     }
 
-    uint8_t* bmp = nullptr;
-    size_t bmpLength = 0;
-
-    bool converted = false;
-    if (fb)
-      converted = frame2bmp(fb, &bmp, &bmpLength);
-
     if(fb) {
         esp_camera_fb_return(fb);
         fb = NULL;
     }
-
-    if (converted) {
-      Serial.println("Convert frame buffer to bmp");
-      Serial.print("Sending ");
-      Serial.println(bmpLength);
-      res = sendBitmap(bmp, bmpLength);
-
-      free(bmp);
-    }
-    else
-      res = 1;
 
     
   
@@ -479,39 +437,6 @@ esp_err_t takePicture() {
 
     return res;
     
-}
-
-const uint32_t chunkSize = 32;
-
-int sendBitmap(uint8_t* bmp, size_t length) {
-  int result = 0;
-
-  uint32_t chunkCount = (uint32_t)ceil((float)length / (float)chunkSize);
-
-  for (uint32_t chunk = 0; chunk  < chunkCount;  ++chunk) {
-
-      Wire.beginTransmission(0x08);
-        
-        uint32_t chunkStart = chunk * chunkSize;
-        uint32_t chunkEnd = chunkStart + chunkSize;
-        if (chunkEnd > length)
-            chunkEnd = length;
-        uint32_t chunkLength = chunkEnd - chunkStart;
-        Wire.write(bmp + chunkStart,  chunkLength);
-      
-      if ((result = checkEndTransmission("Data Chunk")) != 0) {
-        Serial.print("Broke at chunk ");
-        Serial.println(chunk);
-        break;
-      }
-  }
-
-  if (result == 0)
-    Serial.println("Image sent");
-  else
-    Serial.println("Error sending image");
-
-  return result;
 }
 
 int64_t frame_count = 0;
@@ -548,6 +473,11 @@ static esp_err_t get_stream_handler(httpd_req_t *req){
     camera_fb_t * fb = NULL;
     esp_err_t res = ESP_OK;
 
+    sensor_t * s = esp_camera_sensor_get();
+
+    //s->reset(s);
+    s->set_framesize(s, FRAMESIZE_QVGA);
+
     res = httpd_resp_set_type(req, _STREAM_CONTENT_TYPE);
     if(res != ESP_OK){
         return res;
@@ -558,7 +488,9 @@ static esp_err_t get_stream_handler(httpd_req_t *req){
     frame_count = 0;
     frame_time_sum = 0;
 
-    while(!capturing) {
+    light->turnOn();
+
+    while(1) {
 
       int64_t frame_start =  esp_timer_get_time();
   
@@ -566,7 +498,7 @@ static esp_err_t get_stream_handler(httpd_req_t *req){
       
       if (!fb) {
           Serial.println("Camera capture failed");
-          return ESP_FAIL;
+          res = ESP_FAIL;
       }
 
       if (res == ESP_OK) {
@@ -593,6 +525,8 @@ static esp_err_t get_stream_handler(httpd_req_t *req){
 
     }
     
+    light->turnOff();
+
     return res;
 }
 
@@ -603,172 +537,3 @@ void logMemory() {
   log_d("Free PSRAM: %d", ESP.getFreePsram());
 }
 
-*/
-/*
-void setup()
-{
-
-  Serial.begin(115200);
-  Serial.println();
-
-  //SerialBT.begin("m5-timer-cam"); //Bluetooth device name
-
-  if(ESP_OK != init_camera()) {
-    Serial.println("Failed to initialize camera");
-    while (1)
-      ;
-  }
-  Serial.println("Camera initialized");
-  
-  Serial.printf("Connect to %s, %s\r\n", ssid, password);
-
-  led_init(CAMERA_LED_GPIO);
-  //bat_init();
-
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    led_brightness(1024);
-    delay(250);
-    led_brightness(0);
-    delay(250);
-  }
-
-  led_brightness(0);
-
-  //WiFi.softAP(ssid, password);
-
-  //IPAddress IP = WiFi.softAPIP();
-  //Serial.print("AP IP address: ");
-  //Serial.println(IP);
-
-  Serial.println("");
-  Serial.println("WiFi connected");
-
-  httpd_config_t config = HTTPD_DEFAULT_CONFIG();
-
-
-  httpd_uri_t get_image_uri = {
-    .uri       = "/image",
-    .method    = HTTP_GET,
-    .handler   = get_image_handler,
-    .user_ctx  = NULL
-  };
-
-  httpd_uri_t get_stream_uri = {
-    .uri       = "/stream",
-    .method    = HTTP_GET,
-    .handler   = get_stream_handler,
-    .user_ctx  = NULL
-  };
-
-  //Wire.begin();
-  //Wire.setClock(wireClockSpeed);
-
-  Serial.println("Wire started in master mode.");
-
-  config.server_port = 80;
-  Serial.printf("Starting server on port: '%d'\n", config.server_port);
-  if (httpd_start(&camera_http_handle, &config) == ESP_OK) {
-    httpd_register_uri_handler(camera_http_handle, &get_stream_uri);
-    httpd_register_uri_handler(camera_http_handle, &get_image_uri);
-    Serial.println("Camera Ready! Use: ");
-    Serial.print("http://");
-    Serial.print(WiFi.localIP());
-    //Serial.print(IP);
-    Serial.print("/image");
-    Serial.println("");
-    Serial.print("http://");
-    Serial.print(WiFi.localIP());
-    //Serial.print(IP);
-    Serial.print("/stream");
-    Serial.println("");
-  }
-  else
-    Serial.println("Error starting server");
-
-
-  //bat_init();
-  //bat_hold_output();
-  led_brightness(1024);
-
-  startCameraServer();
-
-  led_brightness(0);
-
-}
-
-
-enum Process {
-  touchWait,
-  touchDown,
-  touchUp,
-  start,
-  header,
-  pixels,
-  footer
-};
-
-
-static uint32_t lastTimeSinceBatteryCheck = 0;
-
-void loop() {
-
-
-  if ((millis() - lastTimeSinceBatteryCheck) > 5000) {
-  //  SerialBT.printf("Batt %lu\n", bat_get_voltage());
-    if  (WiFi.status() != WL_CONNECTED) {
-      Serial.println("Restarting...");
-      ESP.restart();
-    }
-    lastTimeSinceBatteryCheck = millis();
-  }
-
-  //delay(5000);
-
-  if (!capturing) {
-    Wire.requestFrom(0x08, 1);
-    Process process = (Process)Wire.read();    // receive a byte as character
-    //Serial.println(process);
-    if (process == Process::start) {
-      capturing = true;
-      Serial.println("takePicture()");
-      takePicture();
-      capturing = false;
-    }
-  }
-
-  if (false && millis() % 10000 == 0) {
-    // Reading temperature or humidity takes about 250 milliseconds!
-    // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-    float h = dht.readHumidity();
-    // Read temperature as Celsius (the default)
-    float t = dht.readTemperature();
-    // Read temperature as Fahrenheit (isFahrenheit = true)
-    float f = dht.readTemperature(true);
-
-    // Check if any reads failed and exit early (to try again).
-    if (!isnan(h) && !isnan(t) && !isnan(f)) {
-
-      // Compute heat index in Fahrenheit (the default)
-      float hif = dht.computeHeatIndex(f, h);
-      // Compute heat index in Celsius (isFahreheit = false)
-      float hic = dht.computeHeatIndex(t, h, false);
-
-      Serial.print(F("Humidity: "));
-      Serial.print(h);
-      Serial.print(F("%  Temperature: "));
-      Serial.print(t);
-      Serial.print(F("°C "));
-      Serial.print(f);
-      Serial.print(F("°F  Heat index: "));
-      Serial.print(hic);
-      Serial.print(F("°C "));
-      Serial.print(hif);
-      Serial.println(F("°F"));
-    }
-  }
-
-  
-}
-*/
