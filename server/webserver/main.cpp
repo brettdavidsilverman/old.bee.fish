610844a6d407ae8d2ec781fca28af558de470fdc
Brett Silverman <brettdavidsilverman@gmail.com>
2021-12-09 15:58:18 +1000

    wip

--- server/webserver/main.cpp
+++ server/webserver/main.cpp
@@ -1,11 +1,54 @@
 #include <iostream>
+#include <stdlib.h>
+
+#include "../test/test.h"
+#include "version.h"
 #include "httpd.h"
 
-int main(int c, char** v)
+
+int main(int argc, const char* argv[])
 {
     using namespace std;
+    using namespace BeeFishTest;
+      
+    std::cout << "Webserver http Server" << std::endl;
+    std::cout 
+    << "C++ run time: "
+        << __cplusplus
+        << std::endl
+    << "Version: "
+        << BEE_FISH_WEBSERVER_VERSION
+        << std::endl;
+        /*
+    << "Database file: "
+        << databaseFile.toUTF8()
+        << std::endl
+    << "Transaction file: "
+        << transactionFile.toUTF8()
+        << std::endl
+    << "Host: "
+        << HOST_NAME
+        << std::endl;
+    */
 
-    serve_forever("8080");
+    if (hasArg(argc, argv, "-test") >= 0)
+    {
+        cout << "Testing HTTP..." << endl;
+        
+        return 0;
+    }
+    
+    std::string port;
+    int portArg;
+
+    if ((portArg = hasArg(argc, argv, "-port")) >= 0)
+    {
+        port = argv[portArg + 1];
+    }
+    else
+        port = "80";
+
+    serve_forever(partial_sort);
 
     return -1;
 }
