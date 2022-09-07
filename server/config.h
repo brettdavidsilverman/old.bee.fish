233f43946b3d8e7c20f0a9178840a3775c487444
Brett Silverman <brettdavidsilverman@gmail.com>
2022-09-07 20:05:21 +1000

    Added conditionals to database files

--- server/config.h
+++ server/config.h
@@ -5,3 +5,18 @@
 #endif
 
 
+#ifndef BEE_FISH_SERVER_LOG_FILE
+   #define BEE_FISH_SERVER_LOG_FILE    HOME_DIRECTORY "/server.log"
+#endif
+
+#ifndef  BEE_FISH_SERVER_ERR_FILE
+   #define BEE_FISH_SERVER_ERR_FILE    HOME_DIRECTORY "/server.err"
+#endif
+
+#ifndef BEE_FISH_DATABASE_FILE
+   #define BEE_FISH_DATABASE_FILE      HOME_DIRECTORY "/bee.fish.data"
+#endif
+
+#ifndef BEE_FISH_TRANSACTION_FILE
+   #define BEE_FISH_TRANSACTION_FILE   HOME_DIRECTORY "/bee.fish.transaction"
+#endif
