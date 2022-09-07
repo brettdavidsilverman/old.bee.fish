04f724e8eec8764ac45dee505d26998f8b9ed16f
Brett Silverman <brettdavidsilverman@gmail.com>
2022-09-07 19:52:57 +1000

    Pushed directories up a dir

--- server/config.h
+++ server/config.h
@@ -5,9 +5,3 @@
 #endif
 
 
-#define BEE_FISH_SERVER_LOG_FILE    HOME_DIRECTORY "/server.log"
-#define BEE_FISH_SERVER_ERR_FILE    HOME_DIRECTORY "/server.err"
-
-#define BEE_FISH_DATABASE_FILE      HOME_DIRECTORY "/bee.fish.data"
-#define BEE_FISH_TRANSACTION_FILE   HOME_DIRECTORY "/bee.fish.transaction"
-
