2912dfae81639f3f0c5385dabafb6be9e899be2c
Brett David Silverman <brettdavidsilverman@gmail.com>
2022-09-15 03:06:20 +0000

    Added formatJSON

---
+++ client/formatJSON.js
@@ -0,0 +1,42 @@
+function formatJSON(json, table) {
+
+    for (var item in json) {
+        var object = json[item];
+
+        var tr = document.createElement("tr");
+        
+        var tdLabel = document.createElement("td");
+        tdLabel.appendChild(document.createTextNode(item));
+        tr.appendChild(tdLabel);
+        
+        var tdValue = document.createElement("td");
+        var value = object.value;
+        switch (typeof(value)) {
+        case "string":
+            tdValue.align = "left";
+            break;
+        case "number":
+            tdValue.align = "right";
+            if (object.precision != undefined) {
+                value = value.toFixed(object.precision);
+            }
+            break;
+        default:
+            tdValue.align = "center";
+        }
+        tdValue.appendChild(document.createTextNode(value));
+        tr.appendChild(tdValue);
+
+        if (object.unit) {
+        var tdUnit = document.createElement("td");
+        tdUnit.appendChild(document.createTextNode(object.unit));
+        tdUnit.align = "left";
+        tr.appendChild(tdUnit);
+        }
+        else {
+        tdValue.colSpan = "2";
+        }
+
+        table.appendChild(tr);
+    }
+}
