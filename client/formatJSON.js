16457e9af07065a0bc0d1067117ea837364dfd4b
Brett David Silverman <brettdavidsilverman@gmail.com>
2022-09-15 05:55:45 +0000

    Migrating back to dev

--- client/formatJSON.js
+++ client/formatJSON.js
@@ -4,38 +4,48 @@ function formatJSON(json, table) {
         var object = json[item];
 
         var tr = document.createElement("tr");
-        
-        var tdLabel = document.createElement("td");
-        tdLabel.appendChild(document.createTextNode(item));
-        tr.appendChild(tdLabel);
-        
-        var tdValue = document.createElement("td");
-        var value = object.value;
-        switch (typeof(value)) {
-        case "string":
-            tdValue.align = "left";
-            break;
-        case "number":
-            tdValue.align = "right";
-            if (object.precision != undefined) {
-                value = value.toFixed(object.precision);
-            }
-            break;
-        default:
-            tdValue.align = "center";
-        }
-        tdValue.appendChild(document.createTextNode(value));
-        tr.appendChild(tdValue);
 
-        if (object.unit) {
-        var tdUnit = document.createElement("td");
-        tdUnit.appendChild(document.createTextNode(object.unit));
-        tdUnit.align = "left";
-        tr.appendChild(tdUnit);
+        if (object.unit == "url") {
+            var td = document.createElement("td");
+            var a = document.createElement("a");
+            a.href = object.value;
+            a.innerHTML = item;
+            td.appendChild(a);
         }
         else {
-        tdValue.colSpan = "2";
+            var tdLabel = document.createElement("td");
+            tdLabel.appendChild(document.createTextNode(item));
+            tr.appendChild(tdLabel);
+            
+            var tdValue = document.createElement("td");
+            var value = object.value;
+            switch (typeof(value)) {
+            case "string":
+                tdValue.align = "left";
+                break;
+            case "number":
+                tdValue.align = "right";
+                if (object.precision != undefined) {
+                    value = value.toFixed(object.precision);
+                }
+                break;
+            default:
+                tdValue.align = "center";
+            }
+            tdValue.appendChild(document.createTextNode(value));
+            tr.appendChild(tdValue);
+    
+            if (object.unit) {
+                var tdUnit = document.createElement("td");
+                tdUnit.appendChild(document.createTextNode(object.unit));
+                tdUnit.align = "left";
+                tr.appendChild(tdUnit);
+            }
+            else
+                tdValue.colSpan = "2";
+    
         }
+        
 
         table.appendChild(tr);
     }
