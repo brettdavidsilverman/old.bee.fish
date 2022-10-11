e8ec21a90909f2d6dc7e1be0f51cc5c8836014e7
Brett Silverman <brettdavidsilverman@gmail.com>
2022-10-11 21:54:52 +1000

    Revert "wip"
    This reverts commit 6de0c8a6bbdced87ed0b61e4bb6577cd145c675a.

--- feebee-cam/feebee-cam.ino
+++ feebee-cam/feebee-cam.ino
@@ -57,6 +57,20 @@ namespace FeebeeCam {
 
         cerr << "Connected to internet" << endl;
 
+        BeeFishBScript::Object object {
+            {"hello", "world"}
+        };
+
+        FeebeeCam::BeeFishStorage storage("/");
+        storage.setItem("test", object);
+
+        BeeFishBScript::Variable variable = storage.getItem("test");
+        
+        cerr << variable << endl;
+
+        while (1)
+            ;
+
         //FeebeeCam::initializeRTC();
 
         //FeebeeCam::downloadFiles(false, true);
