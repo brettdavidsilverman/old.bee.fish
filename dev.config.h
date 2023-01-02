7b0d25312746502b51f6e3155e76ed25a8a91dec
Brett David Silverman <brettdavidsilverman@gmail.com>
2023-01-02 12:05:48 +0000

    Added sample dev.config.h to bee.fish

---
+++ dev.config.h
@@ -0,0 +1,16 @@
+#define HOME_DIRECTORY "/home/brettdavidsilverman"
+
+#define SERVER
+
+#define CERT_FILE "/etc/letsencrypt/live/dev.bee.fish/fullchain.pem"
+
+#define KEY_FILE "/etc/letsencrypt/live/dev.bee.fish/privkey.pem"
+
+#define TMP_DH_FILE HOME_DIRECTORY "/bee.fish/dhparam4096.pem"
+
+#define FILE_SYSTEM_PATH HOME_DIRECTORY "/bee.fish"
+
+#define TEMP_DIRECTORY "/tmp"
+
+#define HOST_NAME "dev.bee.fish"
+#define HOST "https://" HOST_NAME
